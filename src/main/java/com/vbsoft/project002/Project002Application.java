a76b7c2f251b67e1a019e1f23dca8e86e02586bd
lucasbaldusca <lucasbaldusca@hotmail.com>
2020-10-05 22:56:28 -0300

    first commit - project created

---
+++ src/main/java/com/vbsoft/project002/Project002Application.java
@@ -0,0 +1,13 @@
+package com.vbsoft.project002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project002Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vbsoft/project002/Project002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbsoft.project002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
