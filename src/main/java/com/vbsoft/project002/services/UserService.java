d281c3bc9da4eacef8a31a40ccbb7962f3c96852
lucasbaldusca <lucasbaldusca@hotmail.com>
2020-10-14 21:53:30 -0300

    getting User by Id

--- src/main/java/com/vbsoft/project002/services/UserService.java
+++ src/main/java/com/vbsoft/project002/services/UserService.java
@@ -2,10 +2,12 @@ package com.vbsoft.project002.services;
 
 import com.vbsoft.project002.domain.User;
 import com.vbsoft.project002.repository.UserRepository;
+import com.vbsoft.project002.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
     return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado"));
     }
 }
